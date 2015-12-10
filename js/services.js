37a866f59702800e653fe18a3ea54f69cc0c42c5
Danilo Bargen <mail@dbrgn.ch>
2015-12-10 16:29:59 +0100

    Make API_TOKEN configurable

--- js/services.js
+++ js/services.js
@@ -1,12 +1,12 @@
 var app = angular.module('reservationsApp');
 
-app.config(function($resourceProvider, $httpProvider) {
+app.config(function($resourceProvider, $httpProvider, API_TOKEN) {
 
     // Don't strip trailing slashes from calculated URLs
     $resourceProvider.defaults.stripTrailingSlashes = false;
 
     // Add token authorization header to all requests
-    $httpProvider.defaults.headers.common['Authorization'] = 'Token c3d8b0c98a9d6fba21a3ac0566e37a9b89e3b079';
+    $httpProvider.defaults.headers.common['Authorization'] = 'Token ' + API_TOKEN;
 
 });
 
