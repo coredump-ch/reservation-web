20ae0badb4d98badf21ddb3b027e13cffe4b62e8
Danilo Bargen <mail@dbrgn.ch>
2015-12-11 08:18:11 +0100

    Implement saving new reservations

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,17 @@
 var app = angular.module('reservationsApp');
 
-app.controller('ReservationsCtrl', function($scope, Reservation) {
-    /*$scope.reservations = [
-        {
-            'start': new Date(2015, 12, 04, 16, 0, 0),
-            'end': new Date(2015, 12, 04, 22, 30, 0),
-            'name': 'Danilo',
-        },
-        {
-            'start': new Date(2015, 12, 06, 07, 0, 0),
-            'end': new Date(2015, 12, 04, 09, 0, 0),
-            'name': 'Axel',
-        }
-    ];*/
+app.controller('ReservationListCtrl', function($scope, Reservation) {
     $scope.reservations = Reservation.query({}, function(data, responseHeaders) {
         console.log(data)
     });
+});
+
+app.controller('ReservationAddCtrl', function($scope, Reservation) {
+    $scope.reservation = new Reservation();
+
+    $scope.addReservation = function() {
+        $scope.reservation.$save(function() {
+            alert('Saved');
+        });
+    };
 });
