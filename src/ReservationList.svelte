e57d8dfc5f28db63fadf1222069392b4fc743fce
Danilo Bargen <mail@dbrgn.ch>
2020-04-06 00:24:16 +0200

    Show reservations using await block

--- src/ReservationList.svelte
+++ src/ReservationList.svelte
@@ -2,14 +2,10 @@
     import * as moment from 'moment'
     import 'moment-precise-range-plugin'; // Used for `moment().preciseDiff()` to work
 
-    import { onMount } from 'svelte';
-
     import { getReservations }  from './api';
 
     export let API_URL;
     export let API_TOKEN;
-
-    let reservations;
 
     function absoluteDate(m) {
         return m.format('DD.MM.YYYY HH:mm');
@@ -20,30 +16,23 @@
     }
 
     function duration(start, end) {
-        console.log('obj', moment().preciseDiff(start, end));
         return moment.preciseDiff(start, end);
     }
 
-    onMount(async () => {
-        const data = await getReservations(API_URL, API_TOKEN);
-        reservations = data.map(res => {
+    let reservationsPromise = getReservations(API_URL, API_TOKEN)
+        .then(reservations => reservations.map(res => {
             console.log('Reservation:', res);
-
             // Parse dates
             res.start = moment(res.start);
             res.end = moment(res.end);
-
             return res;
-        });
-    });
+        }));
 </script>
 
 <style>
 </style>
 
 <h2>Aktive Reservationen</h2>
-
-<div ng-include="'templates/errors.html'"></div>
 
 <table class="table table-striped table-hover">
     <thead>
@@ -56,15 +45,23 @@
     </thead>
 
     <tbody>
-        {#if reservations}
-        <tr>
-            {#each reservations as res}
-                <td title="{absoluteDate(res.start)}">{relativeDate(res.start)}</td>
-                <td>{duration(res.start, res.end)}</td>
-                <td title="{absoluteDate(res.end)}">{relativeDate(res.end)}</td>
-                <td>{res.owner}</td>
-            {/each}
-        </tr>
-        {/if}
+        {#await reservationsPromise}
+            <tr><td colspan="4"><em>Loading...</em></td></tr>
+        {:then reservations}
+            {#if reservations}
+                {#each reservations as res}
+                    <tr>
+                        <td title="{absoluteDate(res.start)}">{relativeDate(res.start)}</td>
+                        <td>{duration(res.start, res.end)}</td>
+                        <td title="{absoluteDate(res.end)}">{relativeDate(res.end)}</td>
+                        <td>{res.owner}</td>
+                    </tr>
+                {/each}
+            {:else}
+                <tr><td colspan="4"><em>No reservations!</em></td></tr>
+            {/if}
+        {:catch error}
+            <tr><td colspan="4"><em>Error loading data</em></td></tr>
+        {/await}
     </tbody>
 </table>
