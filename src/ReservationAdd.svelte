342035c464acf9291233d6911c295459d46e9e6d
Danilo Bargen <mail@dbrgn.ch>
2020-04-10 17:27:01 +0200

    Add form: Bind fields to variables, parse date / duration

--- src/ReservationAdd.svelte
+++ src/ReservationAdd.svelte
@@ -1,4 +1,16 @@
 <script>
+    import * as moment from 'moment'
+
+    const DATE_FORMAT = 'Y-MM-DD HH:mm';
+
+    // Set initial values
+    let owner = '';
+    let start = moment().format(DATE_FORMAT);
+    let duration = 1;
+
+    // Calculated start and end moments
+    $: startMoment = moment(start);
+    $: endMoment = moment(start).add(moment.duration({hours: duration}));
 </script>
 
 <style>
@@ -6,22 +18,18 @@
 
 <h2>Neue Reservation</h2>
 
-<div class="alert alert-success fade" ng-show="showSuccessMessage">Reservation gespeichert!</div>
-
-<div ng-include="'templates/errors.html'"></div>
-
-<form class="form" role="form" ng-submit="addReservation()">
+<form class="form" role="form">
     <div class="form-group">
         <label for="owner" class="control-label">Name</label>
-        <input id="owner" ng-model="reservation.owner" class="form-control" placeholder="Dein Name">
+        <input id="owner" class="form-control" placeholder="Dein Name" bind:value="{owner}">
     </div>
     <div class="form-group">
         <label for="start" class="control-label">Beginn</label>
-        <input id="start" ng-model="reservation.start" class="form-control" type="datetime-local">
+        <input id="start" class="form-control" type="datetime-local" bind:value="{start}">
     </div>
     <div class="form-group">
-        <label for="end" class="control-label">Dauer (h)</label>
-        <input id="end" ng-model="reservation.duration"  class="form-control" type="number" min="0" max="48" step="0.25" value="1">
+        <label for="duration" class="control-label">Dauer (h)</label>
+        <input id="duration" class="form-control" type="number" min="0" max="48" step="0.25" bind:value="{duration}">
     </div>
     <div class="form-group">
         <button class="btn btn-primary">Go!</button>
