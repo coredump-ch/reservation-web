91b5b433038c58448f37ece532d355485100905e
Danilo Bargen <mail@dbrgn.ch>
2020-04-11 00:06:41 +0200

    Form validation

--- src/ReservationAdd.svelte
+++ src/ReservationAdd.svelte
@@ -14,6 +14,9 @@
 </script>
 
 <style>
+    .form-group.invalid input {
+        border-color: red;
+    }
 </style>
 
 <h2>Neue Reservation</h2>
@@ -21,15 +24,15 @@
 <form class="form" role="form">
     <div class="form-group">
         <label for="owner" class="control-label">Name</label>
-        <input id="owner" class="form-control" placeholder="Dein Name" bind:value="{owner}">
+        <input id="owner" class="form-control" placeholder="Dein Name" bind:value="{owner}" required>
     </div>
-    <div class="form-group">
+    <div class="form-group" class:invalid="{start.length < 16 || !startMoment.isValid()}">
         <label for="start" class="control-label">Beginn</label>
-        <input id="start" class="form-control" type="datetime-local" bind:value="{start}">
+        <input id="start" class="form-control" type="datetime-local" bind:value="{start}" required>
     </div>
-    <div class="form-group">
+    <div class="form-group" class:invalid="{!duration}">
         <label for="duration" class="control-label">Dauer (h)</label>
-        <input id="duration" class="form-control" type="number" min="0" max="48" step="0.25" bind:value="{duration}">
+        <input id="duration" class="form-control" type="number" min="0" max="48" step="0.25" bind:value="{duration}" required>
     </div>
     <div class="form-group">
         <button class="btn btn-primary">Go!</button>
